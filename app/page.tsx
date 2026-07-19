0caf71415d489b25d24f686fcb5d3433b019e7cf
gunasanjaysagar <gunasanjaysagar@gmail.com>
2026-07-19 19:59:08 +0530

    Initial Next.js project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        Welcome to GunaMythicArt s
+      </h1>
+    </main>
+  );
+}
